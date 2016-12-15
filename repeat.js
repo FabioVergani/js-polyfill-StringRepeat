955bed992473f639c8025ffa99397aadb678e780
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-12-15 14:03:04 +0100

    Create repeat.js

---
+++ repeat.js
@@ -0,0 +1,30 @@
+var o=String.prototype, f=o.repeat;
+if(!f){
+ (function(x){'use strict';
+  function r(n){
+   var i=n,t='',s=this;
+   if(s===null){
+    throw TypeError();
+   }else{
+    s=String(s);
+	if(s.length!==0){
+	 i=Number(i);
+	 if((i<0)||(i===Infinity)){
+	  throw RangeError();
+	 }else{
+      if(i>1){while(i){t+=s;--i;};}else{if(i===1){t=s}};
+      s=t;
+	 };
+    };
+    return t;
+   };
+  };
+  if(f=Object.defineProperty){f(x,'repeat',{'value':r,'configurable':true,'writable':true});}else{o.repeat=r;};
+ })(o);
+};
+/*
+console.log("1".repeat(0));
+console.log("1".repeat(11));
+console.log("".repeat(11));
+console.log("".repeat(0));
+*/
